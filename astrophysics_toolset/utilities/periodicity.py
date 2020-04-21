301ba52bc1305ff60c74eaf43f911a7cc148470c
Corentin Cadiou <contact@cphyc.me>
2020-04-21 16:17:47 +0100

    Create "distance to center" function

--- astrophysics_toolset/utilities/periodicity.py
+++ astrophysics_toolset/utilities/periodicity.py
@@ -3,7 +3,7 @@ import numpy as np
 from typing import Optional
 from .types import FloatArrayType
 
-def wrap_coordinates(x : FloatArrayType, w : Optional[float] = 1) -> FloatArrayType:
+def wrap_coordinates(x : FloatArrayType, w : float = 1) -> FloatArrayType:
     """Wrap the position, taking into account periodicity.
     
     Parameters:
@@ -20,3 +20,33 @@ def wrap_coordinates(x : FloatArrayType, w : Optional[float] = 1) -> FloatArrayT
     """
     x_wrapped = np.mod(x, w)
     return x_wrapped
+
+
+def distance_to_center(x : FloatArrayType, w : float = 1) -> FloatArrayType:
+    """Compute the distance to the barycenter, taking into account periodicity.
+    
+    Parameters:
+    -----------
+    x : 3D array
+        The position
+    w : float, optional
+        The width of the domain (default : 1)
+
+    Returns
+    -------
+    dx : 3D array
+        The distance to the center, wrapped with periodic boundaries
+    """
+
+    # Compute distances to first element
+    x0 = x[..., 0, :]
+    dx = x[..., 1:, :] - x0[..., None, :]
+    dx[dx>+w/2] -= w
+    dx[dx<-w/2] += w
+
+    # Now rewrite w.r.t. center
+    xcenter = dx.mean(axis=-2) + x0
+    dx = x - xcenter[..., None, :]
+    dx[dx>+w/2] -= w
+    dx[dx<-w/2] += w
+    return dx
