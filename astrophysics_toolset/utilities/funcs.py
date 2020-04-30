340cab2ab74866326e48bef8ac5a00f235f6c505
Corentin Cadiou <contact@cphyc.me>
2020-04-30 14:34:28 +0100

    Now j1/x is precise down to 1e-14

--- astrophysics_toolset/utilities/funcs.py
+++ astrophysics_toolset/utilities/funcs.py
@@ -5,7 +5,7 @@ import numpy as np
 @vectorize([float64(float64), float32(float32)])
 def j1_over_x(x):
     """Compute j_1(x)/x.
-    
+
     Parameter
     ---------
     x : float, or array-like
@@ -17,7 +17,9 @@ def j1_over_x(x):
     x2 = x*x
     if x < 1e-2:
         return (1 - x2*0.1*(1 - x2/28))/3
-    elif x < 1e-1:
-        return (1 - x2*0.1*(1 - x2/28*(1 - x2/54.)))/3
+    # elif x < 8e-2:
+    #     return (1 - x2*0.1*(1 - x2/28*(1 - x2/54.)))/3
+    elif x < 2e-1:
+        return (1 - x2*0.1*(1 - x2/28*(1 - x2/54. * (1 - x2/88.))))/3
     else:
         return (np.sin(x) / x - np.cos(x))/x2
---
+++ astrophysics_toolset/utilities/tests/test_funcs.py
@@ -0,0 +1,22 @@
+from ..funcs import j1_over_x
+
+from mpmath import sqrt, pi, besselj, mpf
+import numpy as np
+
+
+@np.vectorize
+def mpmath_jn_over_x(i, x):
+    xx = mpf(x)
+    if x == 0:
+        return float(1/mpf(3))
+    else:
+        return float(sqrt(pi/mpf(2)/xx) * besselj(i+mpf("1/2"), xx) / xx)
+
+
+def test_j1_over_x():
+    x = np.concatenate(([0], np.geomspace(1e-8, 10, 1000)))
+
+    yref = mpmath_jn_over_x(1, x)
+    yval = j1_over_x(x)
+
+    np.testing.assert_allclose(yref, yval, rtol=1e-14)
