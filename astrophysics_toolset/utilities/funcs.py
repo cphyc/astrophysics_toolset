a6e682f1c8df78c5cb3c4c0fdbd0a9d8e05d0588
Corentin Cadiou <contact@cphyc.me>
2020-04-30 13:59:16 +0100

    Add funcs.py functino

---
+++ astrophysics_toolset/utilities/funcs.py
@@ -0,0 +1,23 @@
+from numba import vectorize, float64, float32
+import numpy as np
+
+
+@vectorize([float64(float64), float32(float32)])
+def j1_over_x(x):
+    """Compute j_1(x)/x.
+    
+    Parameter
+    ---------
+    x : float, or array-like
+
+    Return
+    ------
+    j_1(x) / x : float, or array-like
+    """
+    x2 = x*x
+    if x < 1e-2:
+        return (1 - x2*0.1*(1 - x2/28))/3
+    elif x < 1e-1:
+        return (1 - x2*0.1*(1 - x2/28*(1 - x2/54.)))/3
+    else:
+        return (np.sin(x) / x - np.cos(x))/x2
