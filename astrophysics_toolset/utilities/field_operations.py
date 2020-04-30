4f0e9da50d4769b21ea3476b96dc17ae661ef9c5
Corentin Cadiou <contact@cphyc.me>
2020-04-30 14:34:57 +0100

    Adding top hat filter

--- astrophysics_toolset/utilities/field_operations.py
+++ astrophysics_toolset/utilities/field_operations.py
@@ -2,6 +2,9 @@
 
 import numpy as np
 
+from scipy.special import spherical_jn
+from .funcs import j1_over_x
+
 
 def gaussian_filter(i, R):
     """Smooth the input on R pixels
@@ -19,10 +22,53 @@ def gaussian_filter(i, R):
         The input array, smoothed by a Gaussian filter
     """
     ik = np.fft.rfftn(i)
-    d = 1/(2*np.pi)
+    d = 1 / (2 * np.pi)
     kgrid = np.stack(
-        np.meshgrid(*[np.fft.fftfreq(_, d=d) for _ in i.shape[:-1]],
-                    np.fft.rfftfreq(i.shape[-1], d=d), indexing='ij'), axis=0)
-    k2 = np.sum(kgrid**2, axis=0)
+        np.meshgrid(
+            *[np.fft.fftfreq(_, d=d) for _ in i.shape[:-1]],
+            np.fft.rfftfreq(i.shape[-1], d=d),
+            indexing="ij"
+        ),
+        axis=0,
+    )
+    k2 = np.sum(kgrid ** 2, axis=0)
 
-    return np.fft.irfftn(ik * np.exp(-k2*(R**2/2)))
+    return np.fft.irfftn(ik * np.exp(-k2 * (R ** 2 / 2)))
+
+
+def top_hat_filter(i, R):
+    """Smooth the input on R pixels using a Top-Hat filter in Fourier space
+
+    Parameters
+    ----------
+    i : np.ndarray
+        The array to smooth
+    R : float
+        The radius of the top hat filter
+
+    Returns
+    -------
+    i_smoothed : np.ndarray
+        The input array, smoothed by a Top-Hat
+    """
+    if i.ndim != 3:
+        raise NotImplementedError(
+            "Top-Hat filtering has not been implemented for dimensions ≠ 3, got %s."
+            % i.ndim
+        )
+    ik = np.fft.rfftn(i)
+    d = 1 / (2 * np.pi)
+    kgrid = np.stack(
+        np.meshgrid(
+            *[np.fft.fftfreq(_, d=d) for _ in i.shape[:-1]],
+            np.fft.rfftfreq(i.shape[-1], d=d),
+            indexing="ij"
+        ),
+        axis=0,
+    )
+    kR = np.linalg.norm(kgrid, axis=0) * R
+    i0 = np.unravel_index(0, kR.shape)
+
+    W = 3 * j1_over_x(kR)
+
+    return np.fft.irfftn(ik * W)
--- astrophysics_toolset/utilities/tests/test_field_operations.py
+++ astrophysics_toolset/utilities/tests/test_field_operations.py
@@ -1,27 +1,80 @@
-from ..field_operations import gaussian_filter
+from ..field_operations import gaussian_filter, top_hat_filter
 
 from scipy.ndimage import gaussian_filter as scipy_gaussian_filter
 from scipy.optimize import curve_fit
 import numpy as np
+import pytest
 
 
-def test_gaussian_filter():
-    N = 1000
-    R = 50
-    x = np.zeros(N)
-    x[N//2] = 10
+N1 = 1000
+R1 = 50
 
-    xref = np.arange(N)
-    yref = scipy_gaussian_filter(x, R, mode='wrap')
-    ynew = gaussian_filter(x, R)
+N3 = 64
+R3 = 20
 
-    def fit_fun(x, C, sigma):
-        return C*np.exp(-(x-N//2)**2/2/sigma**2)
 
-    popt, pcov = curve_fit(fit_fun, xref, yref, p0=[1, 2])
+@pytest.fixture
+def x_1D():
+    x_1D = np.zeros(N1)
+    x_1D[N1//2] = 10
+    return x_1D
 
+
+@pytest.fixture
+def x_3D():
+    x_3D = np.zeros((N3, N3, N3))
+    x_3D[N3//2, N3//2, N3//2] = 10
+    return x_3D
+
+
+def test_gaussian_filter_1(x_1D):
+    xref = np.arange(N1)
+    ynew = gaussian_filter(x_1D, R1)
+
+    def fit_fun(x_1D, C, sigma):
+        return C*np.exp(-(x_1D-N1//2)**2/2/sigma**2)
+
+    popt, pcov = curve_fit(fit_fun, xref, ynew, p0=[1, 2])
     Rmeasured = popt[1]
-    dR = np.diag(np.sqrt(pcov))[1]
+    dR = np.sqrt(pcov[1, 1])
 
-    assert Rmeasured-dR < R < Rmeasured+dR
+    # Test width of curve is correct
+    np.testing.assert_allclose(Rmeasured, R1, rtol=dR, atol=0)
+
+
+def test_gaussian_filter_compare_to_scipy(x_1D, x_3D):
+    for x, R in zip((x_1D, x_3D), (R1, R3)):
+        yref = scipy_gaussian_filter(x, R, mode='wrap')
+        ynew = gaussian_filter(x, R)
+
+        # Compare result to the one using scipy's gaussian filter
+        np.testing.assert_allclose(yref, ynew, atol=1e-4, rtol=0)
+
+
+def test_gaussian_filter_volume(x_1D, x_3D):
+    for x, R in zip((x_1D, x_3D), (R1, R3)):
+        ynew = gaussian_filter(x, R)
+
+        # Test volume conservation
+        np.testing.assert_allclose(ynew.sum(), x.sum())
+
+
+def test_gaussian_filter_3d(x_3D):
+    yref = scipy_gaussian_filter(x_3D, R3, mode='wrap')
+    ynew = gaussian_filter(x_3D, R3)
+
+    # Compare result to the one using scipy's gaussian filter
     np.testing.assert_allclose(yref, ynew, atol=1e-4, rtol=0)
+
+
+def test_top_hat_filter_exception(x_1D):
+    with pytest.raises(NotImplementedError) as excinfo:
+        top_hat_filter(x_1D, R1)
+
+    assert "Top-Hat filtering has not been implemented for dimensions ≠ 3" \
+        in str(excinfo.value)
+
+
+def test_top_hat_filter_volume(x_3D):
+    ynew = top_hat_filter(x_3D, R3)
+    np.testing.assert_allclose(ynew.sum(), x_3D.sum())
