087de8073814952f2158715fc8ed73c4c9f8c49c
Corentin Cadiou <contact@cphyc.me>
2020-05-06 18:09:59 +0100

    Initial commit for yorick reader

---
+++ astrophysics_toolset/io/yorick.py
@@ -0,0 +1,75 @@
+import re
+import pyorick
+
+STRUCT_NAME_RE = re.compile('^struct (\w+) \{$')
+ARRAY_RE = re.compile('^\s*(\w+) (\w+)\((\d+)\);$')
+VAR_RE = re.compile('^\s*(\w+) (\w+);$')
+
+
+class PDBReader:
+    _known_types = {
+        'int': int,
+        'float': float,
+        'pointer': 'yorick_pointer'
+    }
+
+    def __init__(self, fname):
+        self._known_types = self._known_types.copy()
+        self.yo = pyorick.Yorick()
+        self.yo(f'f=openb("{fname}")')
+
+        self._variables = self._get_vars()
+        self._structure = {}
+        for v in self._variables:
+            self._structure[v] = self._parse_struct(v)
+
+    def _get_vars(self):
+        self.yo('ptrs=_get_vars(f)')
+
+        length = int(self.yo.e('numberof(ptrs)')) - 1
+        variables = []
+        for i in range(length):
+            variables.append(*self.yo.e(f'*ptrs({i+1})'))
+        return variables
+
+    def _parse_struct(self, var_name):
+        v = f'f.{var_name}'
+        lines = self.yo.e(f'print(structof({v}))')
+
+        # Now parse the structure
+        remaining = lines[1:-1]  # last line is just }
+
+        variables = {}
+        for line in remaining:
+            tmp = ARRAY_RE.match(line)
+            if tmp:
+                type_name, name, length = tmp.groups()
+                length = int(length)
+            else:
+                type_name, name = VAR_RE.match(line).groups()
+                length = 0
+
+            if type_name not in self._known_types:
+                new_type = self._parse_struct(f'{var_name}.{name}')
+                self._known_types[type_name] = new_type
+                type_name = new_type
+            else:
+                type_name = self._known_types[type_name]
+
+            if length == 0:
+                spec = type_name
+            else:
+                spec = (type_name, length)
+            variables[name] = spec
+        return variables
+
+    @property
+    def structure(self):
+        return self._structure
+
+    def get(self, path):
+        cmd = 'f.%s' % path.replace('/', '.')
+        return self.yo(f'={cmd}')
+
+    def __getitem__(self, key):
+        return self.get(key)
